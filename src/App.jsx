32af58ce66660c3392adf4f4f3057e5bb56f8fd3
Antonio Saqueton <code.antoniohicetasaqueton@gmail.com>
2024-11-25 14:56:40 +0800

    Add global styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,47 @@
+import styled from 'styled-components';
+import GlobalStyles from './styles/GlobalStyles';
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  background-color: yellow;
+`;
+
+const Button = styled.button`
+  font-size: 1.4rem;
+  padding: 1.2rem 1.6rem;
+  font-weight: 500;
+  border: none;
+  border-radius: 7px;
+  background-color: purple;
+  color: white;
+  cursor: pointer;
+  margin: 20px;
+`;
+
+const Input = styled.input`
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  padding: 0.8rem 1.2rem;
+`;
+
+const StyledApp = styled.main`
+  background-color: orangered;
+  padding: 20px;
+`;
+
 function App() {
-  return <h1>Hello React!</h1>;
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <H1>The Wild Oasis</H1>
+        <Button onClick={() => alert('Check in!')}>Check In</Button>
+        <Button onClick={() => alert('Check out!')}>Check Out</Button>
+        <Input type='number' placeholder='Number of guests' />
+      </StyledApp>
+    </>
+  );
 }
 
 export default App;
