5ce8d38a513116140a3327e45c467c4bc6570d9e
Antonio Saqueton <code.antoniohicetasaqueton@gmail.com>
2024-11-28 18:30:10 +0800

    Add useQuery and useMutation notification toast

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-// import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import {
   createBrowserRouter,
   RouterProvider,
@@ -6,6 +5,7 @@ import {
 } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { Toaster } from 'react-hot-toast';
 
 import GlobalStyles from './styles/GlobalStyles';
 
@@ -51,6 +51,26 @@ function App() {
       <ReactQueryDevtools initialIsOpen={false} />
       <GlobalStyles />
       <RouterProvider router={router} />
+      <Toaster
+        position='top-center'
+        gutter={12}
+        containerStyle={{ margin: '.8rem' }}
+        toastOptions={{
+          success: {
+            duration: 3000,
+          },
+          error: {
+            duration: 5000,
+          },
+          style: {
+            fontSize: '1.6rem',
+            maxWidth: '50rem',
+            padding: '1.6rem 2.4rem',
+            backgroundColor: 'var(--color-grey-0)',
+            color: 'var(--color-grey-700)',
+          },
+        }}
+      />
     </QueryClientProvider>
   );
 }
